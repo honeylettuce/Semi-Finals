47da982d27a28cddb95a75c4e029d279310ff65f
honeylettuce <143990919+honeylettuce@users.noreply.github.com>
2023-12-03 20:01:34 +0800

    Cancellation of modes

--- COLOR_GAME_2.js
+++ COLOR_GAME_2.js
@@ -7,8 +7,7 @@ let colorDisplay = document.querySelector("#color-display");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
-let modeButtons = document.querySelectorAll(".mode");
-let easyButton = document.querySelector(".mode");
+
 
 init();
 
@@ -40,21 +39,7 @@ function setupSquares() {
 		});
 	}
 }
-/*Mode for the game*/
-function setupMode() {
-	for(let i = 0; i < modeButtons.length; i++) {
-		modeButtons[i].addEventListener("click", function() {
-			for (let i = 0; i < modeButtons.length; i++) {
-				modeButtons[i].classList.remove("selected");
-			}
-			this.classList.add("selected");
-			if (this.textContent === "Easy") {
-				numSquares = 3;
-			}
-			reset();
-		});
-	}
-}
+
 
 function reset() {
 	colors = genRandomColors(numSquares);
